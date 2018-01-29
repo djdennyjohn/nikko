9b1b63d3de15282dd4a70e86fd373ff7f2082cf8
DENNIZ :) <apple@Dennys-MacBook-Air.local>
2018-01-29 14:47:17 +0530

    fix

--- js/directive/background-image-directive.js
+++ js/directive/background-image-directive.js
@@ -1,9 +1,10 @@
 'use strict';
 app.directive('backgroundImageDirective',[ function () {
     return function (scope, element, attrs) {
-      if(attrs.backgroundImageDirective == 'posterthatismissing.jpg'){
+      if(attrs.backgroundImageDirective === 'posterthatismissing.jpg'){
         element.attr({
             'src': './Slices/placeholder_for_missing_posters.png',
+            'class' : 'missing-poster',
         });
       }
        else {
